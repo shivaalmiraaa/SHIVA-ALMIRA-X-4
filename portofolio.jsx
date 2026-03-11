8da9ff29def25f51a6d2b886d081db0c52f79fdb
unknown <shivaalmiraaa@gmail.com>
2026-03-11 14:39:16 +0700

    initial commit

---
+++ portofolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function portofolio() {
+  return (
+    <div>
+      <h1>portofolio shiva almira x4 </h1>
+      <p>hello,ini adalah portofolio saya. </p>
+    </div>
+  )
+}
+
+export default portofolio
